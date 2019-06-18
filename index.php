999979800ca4991509601ff45eb155306d9afc1f
Anderson <anderson.alves@webcontabil.com.br>
2019-06-18 08:41:03 -0300

    Arquivo teste para iniciar aplicação

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+use App\Controllers\Controller;
+
+require 'vendor/autoload.php';
+
+
+
+/*
+ * Arquivo teste para carregar e testar as fuções criadas no view, controller e model
+ *
+ *
+ * teste de instancia
+ * $controller = new App\Controllers\Controller();
+ * $data = $controller->index();
+ *
+ */
